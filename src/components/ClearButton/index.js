4ac47c407405aba98a56dc86ea59f69fdd66f23a
Daniel Park <itsdpark@fb.com>
2017-07-13 22:10:14 -0700

    clear button styles

--- src/components/ClearButton/index.js
+++ src/components/ClearButton/index.js
@@ -3,11 +3,52 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
     background-color: transparent;
+    position: relative;
 
     h1 {
       font-size: 1.3rem;
       color: pink;
     }
+
+    .clear_button {
+      position: relative;
+      height: 53px;
+      width: 123px;
+    }
+
+    .clear_button:hover .shadow {
+      left: 0; 
+      top: 0; 
+    }
+
+    .clear {
+      width: 120px;
+      height: 50px;
+      background: black;
+      color: white;
+      -webkit-box-shadow: none;
+      border: 2px solid white;
+      position: absolute;
+      left: 0; 
+      top: 0; 
+      z-index:1;
+      font-family: 'Source Code Pro', monospace;
+      outline: none;
+    }
+
+    .shadow {
+      position: absolute;
+      width: 120px;
+      height: 50px;
+      background: white;
+      border: 2px solid white;
+      left: 3px; 
+      top: 3px;
+      transition: all 0.3s ease;
+    }
+
+
+
 `
 
 class ClearButton extends Component {
@@ -28,7 +69,10 @@ class ClearButton extends Component {
     render() {
       return (
           <Wrapper {...this.props}>
-            <h1>BASE COMPONENT</h1>
+            <div className="clear_button">
+              <button className="clear" onClick={this.clearMusic}>clear</button>
+              <button className="shadow"></button>
+            </div> 
           </Wrapper>
       )
     }
